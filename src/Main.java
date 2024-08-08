463ddb0f814e41003da43fb16a55c899f3bd1b01
Karol Pietrów <karolpietrow2004@gmail.com>
2024-08-08 17:10:50 +0200

    zad3

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,13 @@ public class Main {
         Point p2 = new Point(6,0);
         Segment s1 = new Segment(p1,p2);
         System.out.println("Długość odcinka: " + s1.getLength());
-        System.out.println("KOD: " + s1.toSvg());
+
+        Point p3 = new Point(1,1);
+        Segment[] perpendiculars = Segment.perpendicular(s1, p3);
+
+        for (Segment s : perpendiculars) {
+            System.out.println("Punkt początkowy: (" + s.getStart().x + ", " + s.getStart().y + ")");
+            System.out.println("Punkt końcowy: (" + s.getEnd().x + ", " + s.getEnd().y + ")");
+        }
     }
 }
